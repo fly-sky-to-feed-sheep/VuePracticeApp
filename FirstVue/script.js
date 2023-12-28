01229eb42f235b3ccc67a3837413bd82856b0441
fly-sky-to-feed-sheep <yingjunjiang57@gmail.com>
2023-12-28 12:42:01 +0800

    v0,5

--- FirstVue/script.js
+++ FirstVue/script.js
@@ -4,12 +4,51 @@ new Vue({
 
     data(){
         return{
-            content:'This is a note.',
+            content: localStorage.getItem('content')||'fake typora',
+            notes:[],
+            selectedId: null,
         }
     },
     computed:{
         notePreview(){
-            return marked.marked(this.content)
+            return this.selectedNote ? marked.marked(this.selectedNote.content) : ' '
+        },
+        selectedNote()
+        {
+            return this.notes.find(note => note.id === this.selectedId) || { content: '' };
         },
     },
+    watch:{
+        content:{
+            handler: 'saveNote',
+        },
+    },
+    methods:{
+        saveNote(val){
+            console.log('saving note:',val)
+            localStorage.setItem('content',val)
+            this.reportOperation('saving')
+        },
+        reportOperation(opName){
+            console.log('The',opName,'operation was completed')
+        },
+        addNote(){
+            const time = Date.now()
+            const note = {
+                id: String(time),
+                title:'New note ' + (this.notes.length +1),
+                content: '**Hi** This notebook is using [markdown](https://github.com/markedjs/marked) for formatting',
+                created: time,
+                favorite: false,
+            }
+            this.notes.push(note)
+        },
+        selectNote(note) {
+            this.selectedId = note.id;
+        },
+    },
+
+    mounted(){
+        console.log('restore note:',localStorage.getItem('content'))
+    }
 })
